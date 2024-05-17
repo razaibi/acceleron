97f761cbc2f333d95c9209a9c44954e0fe02014f
Raza Balbale <ribalbale@gmail.com>
2024-05-17 00:48:31 -0500

    Removed wrongly formatted project.

--- go/go-bare-webapi/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-type ContextKey int
-
-const (
-	UserClaimsKey ContextKey = iota // iota initializes 0, increments by 1 for each iota
-)
-
-var JwtKey = []byte("my_secret_key")
--- go/go-bare-webapi/models/claims.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/dgrijalva/jwt-go"
-
-type Claims struct {
-	Username string   `json:"username"`
-	Roles    []string `json:"roles"`
-	jwt.StandardClaims
-}
